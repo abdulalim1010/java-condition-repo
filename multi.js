bd63903355937cf65fb709856ed2c5610f56ad63
abdulalilm1010 <abdulalim243457@gmail.com>
2025-02-06 23:15:54 +0600

    last time

---
+++ multi.js
@@ -0,0 +1,22 @@
+
+/* 
+if (salary > 30000 && isBcs) {
+  console.log("good patro")
+  
+}
+else {
+  console.log("bad patro");
+
+} */
+const salary =63000;
+const isBcs = true;
+const height = 5.6;
+
+if (salary > 30000 && isBcs==true && height>5.5) {
+  console.log("good patro");
+  
+}
+else {
+  console.log("bad patro");
+
+}
